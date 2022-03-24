410d7ec9e44b01bf475cd9466311535590cf96a7
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2022-03-24 17:03:05 +0900

    add no.7

--- Main.java
+++ Main.java
@@ -19,22 +19,7 @@ class Main {
     System.out.print("体重(kg)：");
     double weight = scanner.nextDouble();
     
-    printData(firstName, lastName, age, height, weight);
-  }
-  
-  public static void printData(String firstName, String lastName, int age, double height, double weight) {
-    System.out.println("名前は" + fullName(firstName, lastName) + "です");
-    System.out.println("年齢は" + age + "歳です");
-    if (age >= 20) {
-      System.out.println("成年者です");
-    } else {
-      System.out.println("未成年者です");
-    }
-    System.out.println("身長は" + height + "mです");
-    System.out.println("体重は" + weight + "kgです");
-  }
-  
-  public static String fullName(String firstName, String lastName) {
-    return firstName + " " + lastName;
+    Person.printData(firstName, lastName, age, height, weight);
+    // 他のクラスのメソッドを呼び出すには、クラス名.メソッド名();
   }
 }
